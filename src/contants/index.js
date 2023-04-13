6bcf0cdf332e9c4f564a6898d8c4160aed889873
CodingLinhTinh <ngocquachgamedevz@gmail.com>
2023-04-13 12:02:54 +0700

    update folders

---
+++ src/contants/index.js
@@ -0,0 +1,5 @@
+import Colors from './Colors';
+import Images from './Images';
+import WelcomeText from './WelcomeText';
+
+export {Colors, Images, WelcomeText};
---
+++ src/screens/index.js
@@ -0,0 +1,3 @@
+import SplashScreen from "./SpalshScreen";
+
+export {SplashScreen}
